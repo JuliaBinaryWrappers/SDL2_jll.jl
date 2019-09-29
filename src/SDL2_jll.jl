6ba9940487f7b9cf07ed856038b8991035aacb22
Elliot Saba <staticfloat@gmail.com>
2019-09-29 15:37:11 -0400

    SDL2_jll build 2.0.10+0

---
+++ src/SDL2_jll.jl
@@ -0,0 +1,31 @@
+module SDL2_jll
+using Pkg, Pkg.BinaryPlatforms, Pkg.Artifacts, Libdl
+import Base: UUID
+
+# We put these inter-JLL-package API values here so that they are always defined, even if there
+# is no underlying wrapper held within this JLL package.
+const PATH_list = String[]
+const LIBPATH_list = String[]
+
+# Load Artifacts.toml file
+artifacts_toml = joinpath(@__DIR__, "..", "Artifacts.toml")
+
+# Extract all platforms
+artifacts = Pkg.Artifacts.load_artifacts_toml(artifacts_toml; pkg_uuid=UUID("ab825dc5-c88e-5901-9575-1e5e20358fcf"))
+platforms = [Pkg.Artifacts.unpack_platform(e, "SDL2", artifacts_toml) for e in artifacts["SDL2"]]
+
+# Filter platforms based on what wrappers we've generated on-disk
+platforms = filter(p -> isfile(joinpath(@__DIR__, "wrappers", triplet(p) * ".jl")), platforms)
+
+# From the available options, choose the best platform
+best_platform = select_platform(Dict(p => triplet(p) for p in platforms))
+
+# Silently fail if there's no binaries for this platform
+if best_platform === nothing
+    @debug("Unable to load SDL2; unsupported platform $(triplet(platform_key_abi()))")
+else
+    # Load the appropriate wrapper
+    include(joinpath(@__DIR__, "wrappers", "$(best_platform).jl"))
+end
+
+end  # module SDL2_jll

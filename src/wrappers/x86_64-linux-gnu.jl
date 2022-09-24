4a3cc2d37c0b86a11980c6229736a5ecc33e1fbd
jlbuild <juliabuildbot@gmail.com>
2022-09-24 23:11:14 +0000

    SDL2_jll build 2.0.20+1

--- src/wrappers/x86_64-linux-gnu.jl
+++ src/wrappers/x86_64-linux-gnu.jl
@@ -9,10 +9,11 @@ using Xorg_libXrandr_jll
 using Xorg_libXScrnSaver_jll
 using Libglvnd_jll
 using alsa_jll
+using alsa_plugins_jll
 JLLWrappers.@generate_wrapper_header("SDL2")
 JLLWrappers.@declare_library_product(libsdl2, "libSDL2-2.0.so.0")
 function __init__()
-    JLLWrappers.@generate_init_header(Xorg_libX11_jll, Xorg_libXcursor_jll, Xorg_libXext_jll, Xorg_libXinerama_jll, Xorg_libXrandr_jll, Xorg_libXScrnSaver_jll, Libglvnd_jll, alsa_jll)
+    JLLWrappers.@generate_init_header(Xorg_libX11_jll, Xorg_libXcursor_jll, Xorg_libXext_jll, Xorg_libXinerama_jll, Xorg_libXrandr_jll, Xorg_libXScrnSaver_jll, Libglvnd_jll, alsa_jll, alsa_plugins_jll)
     JLLWrappers.@init_library_product(
         libsdl2,
         "lib/libSDL2.so",
